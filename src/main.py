c1ca715399e73c2bc312d861fe1c42a1398a847d
Jed <jedwalton98@gmail.com>
2023-03-13 18:09:08 +0000

    added .gitignore

--- src/main.py
+++ src/main.py
@@ -4,4 +4,3 @@ from src.feature_extractor_package import feature_extractor, IO
 if __name__ == '__main__':
     pdf = IO.read_pdf_from_file('./resources/assembly.pdf')
     feature_extractor.extract_parts_list(pdf)
-
