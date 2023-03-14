a02caadb5b725fb2096b8087e1933f8f81b22e4f
Jed Walton <jedwalton98@gmail.com>
2023-03-14 21:22:19 +0000

    +

--- test/test_feature_extractor_package/test_extract_parts_list/test_part.py
+++ test/test_feature_extractor_package/test_extract_parts_list/test_part.py
@@ -5,7 +5,7 @@ from src.feature_extractor_package.extract_parts_list.part import Part
 
 def test_part():
     part = Part('item', 'qty', 'part_number', 'material')
-    assert part.item == 'item'
-    assert part.qty == 'qty'
-    assert part.part_number == 'part_number'
-    assert part.material == 'material'
+    assert part.item == 'item', "Item not read correctly"
+    assert part.qty == 'qty', "Qty not read correctly"
+    assert part.part_number == 'part_number', "Part Number not read correctly"
+    assert part.material == 'material', "Material not read correctly"
--- test/test_feature_extractor_package/test_feature_extractor.py
+++ test/test_feature_extractor_package/test_feature_extractor.py
@@ -11,20 +11,20 @@ def test_read_pdf_and_extract_parts_list():
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
-                assert row[0] == 'Item'
-                assert row[1] == 'Qty'
-                assert row[2] == 'Part Number'
-                assert row[3] == 'Material'
+                assert row[0] == 'Item', "Heading, Item, not found in first row"
+                assert row[1] == 'Qty', "Heading, Qty, not found in first row"
+                assert row[2] == 'Part Number', "Heading, Part Number, not found in first row"
+                assert row[3] == 'Material', "Heading, Material, not found in first row"
             elif line_count == 1:
-                assert row[0] == '27'
-                assert row[1] == '4'
-                assert row[2] == 'M5 Cap Screw(4)'
-                assert row[3] == 'Stainless Steel'
+                assert row[0] == '27', "Value, 27, not found in second row"
+                assert row[1] == '4', "Value, 4, not found in second row"
+                assert row[2] == 'M5 Cap Screw(4)', "Value, M5 Cap Screw(4), not found in second row"
+                assert row[3] == 'Stainless Steel', "Value, Stainless Steel, not found in second row"
             elif line_count == 28:
-                assert row[0] != 'Item'
-                assert row[1] != 'Qty'
-                assert row[2] != 'Part Number'
-                assert row[3] != 'Material'
+                assert row[0] != 'Item', "Heading, Item, found in last row. Should not be there."
+                assert row[1] != 'Qty', "Heading, Qty, found in last row. Should not be there."
+                assert row[2] != 'Part Number', "Heading, Part Number, found in last row. Should not be there."
+                assert row[3] != 'Material', "Heading, Material, found in last row. Should not be there."
             line_count += 1
 
         os.remove('./test/resources/csv/parts_list.csv')
