c6453a44dbd2c35679b2bb75301638c50f491c95
Jed Walton <jedwalton98@gmail.com>
2023-03-14 20:43:24 +0000

    file not found error

--- test/test_feature_extractor_package/test_extract_parts_list/test_parts_utils.py
+++ test/test_feature_extractor_package/test_extract_parts_list/test_parts_utils.py
@@ -15,33 +15,37 @@ from src.feature_extractor_package.extract_parts_list.parts_utils import *
 #     assert part_number[0] == 'Part Number'
 #     assert material[0] == 'Material'
 
+def is_string_present_in_array(npArray, string):
+    for element in npArray:
+        if element == string:
+            return True
 
-def test_parse_parts_lists_headings():
-    item = np.array(['1', '2', '3', 'Item'])
-    qty = np.array(['1', '2', '3', 'Qty'])
-    part_number = np.array(['1', '2', '3', 'Part Number'])
-    material = np.array(['1', '2', '3', 'Material'])
-    parts = parse_parts_lists(item, qty, part_number, material)
-    assert parts[len(parts) - 1].item != 'Item'
-    assert parts[len(parts) - 1].qty != 'Qty'
-    assert parts[len(parts) - 1].part_number != 'Part Number'
-    assert parts[len(parts) - 1].material != 'Material'
+    return False
+
+
+def test_extract_raw_parts_list_data_from_pdf():
+    pdf = read_pdf_from_file('../../resources/assembly/assembly.pdf')
+    item, qty, part_number, material = extract_raw_parts_list_data_from_pdf(pdf)
+    assert is_string_present_in_array(item, 'Item'), "Item not found in item array"
+    assert is_string_present_in_array(qty, 'Qty'), "Qty not found in qty array"
+    assert is_string_present_in_array(part_number, 'Part Number'), "Part Number not found in part_number array"
+    assert is_string_present_in_array(material, 'Material'), "Material not found in material array"
 
 
 def test_parse_parts_lists():
-    item = np.array(['1', '2', '3', 'Item'])
-    qty = np.array(['1', '2', '3', 'Qty'])
-    part_number = np.array(['1', '2', '3', 'Part Number'])
-    material = np.array(['1', '2', '3', 'Material'])
+    item = np.array(['1', '2', '3'])
+    qty = np.array(['1', '2', '3'])
+    part_number = np.array(['1', '2', '3'])
+    material = np.array(['1', '2', '3'])
     parts = parse_parts_lists(item, qty, part_number, material)
-    assert parts[0].item == '1'
-    assert parts[0].qty == '1'
-    assert parts[0].part_number == '1'
-    assert parts[0].material == '1'
+    assert parts[0].item == '1', "Item not read correctly in parts[0].item array"
+    assert parts[0].qty == '1', "Qty not read correctly in parts[0].qty array"
+    assert parts[0].part_number == '1', "Part Number not read correctly in parts[0].part_number array"
+    assert parts[0].material == '1', "Material not read correctly in parts[0].material array"
 
-    assert item[len(parts) - 1] == '3'
-    assert qty[len(parts) - 1] == '3'
-    assert part_number[len(parts) - 1] == '3'
-    assert material[len(parts) - 1] == '3'
+    assert item[len(parts) - 1] == '3', "Item not found in item array"
+    assert qty[len(parts) - 1] == '3', "Qty not found in qty array"
+    assert part_number[len(parts) - 1] == '3', "Part Number not found in part_number array"
+    assert material[len(parts) - 1] == '3', "Material not found in material array"
 
-    assert len(parts) == 3
+    assert len(parts) == 3, "Parts list not parsed correctly"
