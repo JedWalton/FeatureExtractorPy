01fa595abdafeb47599369d95d2407c491643d63
Jed Walton <jedwalton98@gmail.com>
2023-03-14 19:48:59 +0000

    Testing functionality

--- test/test_feature_extractor_package/test_extract_parts_list/test_parts_utils.py
+++ test/test_feature_extractor_package/test_extract_parts_list/test_parts_utils.py
@@ -1,3 +1,4 @@
+
 from src.feature_extractor_package.extract_parts_list.extract_parts_list import read_pdf_from_file
 from src.feature_extractor_package.extract_parts_list.parts_utils import *
 
@@ -21,17 +22,32 @@ def test_get_last_element():
 #     assert material[0] == 'Material'
 
 
-def test_parse_parts_lists():
-    item = ['item1', 'item2']
-    qty = ['qty1', 'qty2']
-    part_number = ['part_number1', 'part_number2']
-    material = ['material1', 'material2']
+def test_parse_parts_lists_headings():
+    item = np.array(['1', '2', '3', 'Item'])
+    qty = np.array(['1', '2', '3', 'Qty'])
+    part_number = np.array(['1', '2', '3', 'Part Number'])
+    material = np.array(['1', '2', '3', 'Material'])
     parts = parse_parts_lists(item, qty, part_number, material)
-    assert parts[0].item == 'item1'
-    assert parts[0].qty == 'qty1'
-    assert parts[0].part_number == 'part_number1'
-    assert parts[0].material == 'material1'
-    assert parts[1].item == 'item2'
-    assert parts[1].qty == 'qty2'
-    assert parts[1].part_number == 'part_number2'
-    assert parts[1].material == 'material2'
+    assert parts[len(parts) - 1].item != 'Item'
+    assert parts[len(parts) - 1].qty != 'Qty'
+    assert parts[len(parts) - 1].part_number != 'Part Number'
+    assert parts[len(parts) - 1].material != 'Material'
+
+
+def test_parse_parts_lists():
+    item = np.array(['1', '2', '3', 'Item'])
+    qty = np.array(['1', '2', '3', 'Qty'])
+    part_number = np.array(['1', '2', '3', 'Part Number'])
+    material = np.array(['1', '2', '3', 'Material'])
+    parts = parse_parts_lists(item, qty, part_number, material)
+    assert parts[0].item == '1'
+    assert parts[0].qty == '1'
+    assert parts[0].part_number == '1'
+    assert parts[0].material == '1'
+
+    assert item[len(parts) - 1] == '3'
+    assert qty[len(parts) - 1] == '3'
+    assert part_number[len(parts) - 1] == '3'
+    assert material[len(parts) - 1] == '3'
+
+    assert len(parts) == 3
